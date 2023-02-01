1ba5e7c43bcea40ed0bacd330353867537c7ace3
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-02-01 23:11:17 +0300

    edits after review №2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
         'posts': posts, }
     return render(request, 'posts/index.html', context)
@@ -11,7 +12,7 @@ def index(request):
 
 def group(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group)[:10]
     context = {
         'group': group,
         'posts': posts, }
