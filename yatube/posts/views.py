b8480ac8b258a63d286b6ca5c4218f49bc22cc1d
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-02-02 03:13:22 +0300

    edits after review №3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()[:settings.OUTPUT_LIMIT]
     context = {
         'posts': posts, }
     return render(request, 'posts/index.html', context)
@@ -12,7 +13,7 @@ def index(request):
 
 def group(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = Post.objects.filter(group=group)[:settings.OUTPUT_LIMIT]
     context = {
         'group': group,
         'posts': posts, }
